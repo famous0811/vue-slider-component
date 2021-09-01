998dc8728ae58a7cc78f5cea0456c7551ce3b10a
allblack <umh0811@naver.com>
2021-09-01 23:12:16 +0900

    vue-slider-asset

---
+++ src/views/slider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button></button>
+    <div></div>
+    <button></button>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+@Options({
+  data() {
+    return {
+      destefsad: [],
+    };
+  },
+})
+export default class Slider extends Vue {}
+</script>
+
+<style lang=""></style>
