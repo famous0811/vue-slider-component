0a5600798a8d43145ed248d9faee748f664357be
allblack <umh0811@naver.com>
2021-09-05 17:12:46 +0900

    make select

--- src/views/select.vue
+++ src/views/select.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="wrap"></div>
+  <div class="wrap">
+    <div class="select__template" ref="dropdown">
+      <div class="select__template__activator" @click="downSelect">
+        {{ selectName }}
+        <i class="iconify" data-icon="mdi:chevron-down" />
+      </div>
+      <transition>
+        <div class="select__options" v-if="selectDown">
+          <div
+            v-for="(item, idx) in selectValue"
+            :key="idx"
+            class="select__options__item"
+            @click="select(item)"
+          >
+            <input
+              type="checkbox"
+              :checked="
+                selectedOptions.findIndex((_option) => _option === item) !== -1
+              "
+              class="select__options__item__checkbox"
+            />
+            <div class="select__options__item__check">
+              <i
+                class="app-dropdown__dropdown__item__check__icon iconify"
+                data-icon="mdi:done"
+              ></i>
+            </div>
+            {{ item }}
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Vue, prop } from "vue-class-component";
@@ -14,6 +46,30 @@ class Props {
 }
 export default class Select extends Vue.with(Props) {
   selectedOptions: string[] = [];
+  selectedOption = false;
+  selectDown = false;
+  downSelect() {
+    this.selectDown = !this.selectDown;
+  }
+  select(option: string) {
+    const idx = this.selectedOptions.findIndex((_option) => _option === option);
+    if (idx !== -1) {
+      this.selectedOptions.splice(idx, 1);
+    } else {
+      this.selectedOptions.push(option);
+    }
+    // console.log(this.selectedOptions);
+  }
+  created() {
+    window.addEventListener("click", (e: MouseEvent) => {
+      if (
+        this.$refs.dropdown &&
+        !(this.$refs.dropdown as any).contains(e.target)
+      ) {
+        this.selectDown = false;
+      }
+    });
+  }
 }
 </script>
 <style lang="scss">
@@ -29,66 +85,74 @@ export default class Select extends Vue.with(Props) {
   height: 100vh;
 }
 .select__template {
-  width: 100%;
-  height: 30px;
-  padding: 10px 20px;
-  border-radius: 8px;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   position: relative;
-  cursor: pointer;
-  transition: all 0.5s;
-  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.2);
-  &:hover {
-    background: $white;
+  width: 100%;
+  &__activator {
+    width: 100%;
+    padding: 15px 20px;
+    border-radius: 8px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    transition: all 0.5s;
+    box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.2);
+    &:hover {
+      background: $white;
+    }
+    & > .iconify {
+      font-size: 24px;
+    }
   }
 }
+
 .select__options {
   position: absolute;
   left: 0;
-  top: 45px;
+  top: 50px;
   max-height: 500px;
   background-color: white;
-  display: none;
+  box-shadow: 0 3px 15px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
   overflow-y: scroll;
   width: 180px;
-  &.show {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-}
-.select_item {
-  box-sizing: border-box;
-  width: 100%;
-  height: 40px;
-  padding: 0 10px;
-  transition: all 0.5s;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  cursor: pointer;
-  &:hover {
-    background: $white;
-  }
-  & > * {
-    cursor: pointer;
-  }
-}
-.select_item_checked {
-  width: 18px;
-  height: 18px;
-  border-radius: 5px;
-  border: 1px solid black;
-  margin: 0 10px;
-  position: relative;
 
-  & > * {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  &__item {
+    box-sizing: border-box;
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    transition: all 0.5s;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    &:hover {
+      background: $white;
+    }
+    &__checkbox {
+      display: none;
+
+      &:checked + .select__options__item__check {
+        .iconify {
+          transform: translateY(0) scale(1);
+        }
+      }
+    }
+    &__check {
+      border: 2px solid black;
+      border-radius: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0 10px;
+      .iconify {
+        transform: translateY(10px) scale(0);
+      }
+    }
   }
 }
 </style>
